93cb797e2d87e8ada86ea2a8ebc93a2119f7228b
guyinatuxedo <ryancmeinke@gmail.com>
2020-02-05 19:50:00 -0800

    Implemented branch analysis value siize

---
+++ 1_callFunc-unit-tests/1_64/callFunc-1-x64.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char *binsh0 = "set me free";
+char *binsh1 = "/bin/sh";
+
+void systemFunct(void)
+{
+        system("echo 'hi'");
+}
+
+void vuln(void)
+{
+	char hi[20];
+	fgets(hi, 100, stdin);
+}	
+
+
+void main(void)
+{
+	vuln();
+}
