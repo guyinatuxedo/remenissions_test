71accdfaaeab15c0ac95fe095dd29c55bc2ebf15
guyinatuxedo <ryancmeinke@gmail.com>
2020-02-06 12:45:36 -0800

    All callfuc basic testcases done

---
+++ 1_callFunc-unit-tests/4_32/callFunc-4-x86.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void win0(void)
+{
+	system("/bin/sh");
+}
+
+void win1(void)
+{
+	system("/bin/sh");
+}
+
+void win2(void)
+{
+	system("/bin/sh");
+}
+
+void vuln(void)
+{
+	char vuln[20];
+	int var0, var1, var2, var3;
+
+	fgets(vuln, 100, stdin);
+
+	if (var0 != 0xfacade)
+	{
+		exit(0);
+	}
+
+	if (var1 != 0xdead)
+	{
+		exit(0);
+	}
+
+	if (var3 != 0xdeadbeef)
+	{
+		exit(0);
+	}
+
+}
+
+void main(void)
+{
+	vuln();
+}
