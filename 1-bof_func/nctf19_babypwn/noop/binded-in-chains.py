388fa05d2a75331462ebbdd69009bd78d69351cf
guyinatuxedo <ryancmeinke@gmail.com>
2020-03-21 02:44:38 -0700

    begun work on argc input

--- 1-bof_func/nctf19_babypwn/noop/binded-in-chains.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-import time
-
-import sys
-
-target = process("./chall-test_nctf19-babypwn")
-gdb.attach(target, gdbscript="get_libc_puts_address")
-
--- 1-bof_func/tu19_thefirst/noop/binded-in-chains.py
+++
@@ -1,5 +0,0 @@
-from pwn import *
-
-import sys
-
-target = process("./chall-test_thefirst")
