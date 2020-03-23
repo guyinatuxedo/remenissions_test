fc44ea746e8171806f1172ac207feabe9d96a595
guyinatuxedo <ryancmeinke@gmail.com>
2020-03-22 23:07:36 -0700

    More argv work

---
+++ 0_var-unit-tests/9_32/var-9-x86.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[])
+{
+	char buf[50];
+	int target;
+
+	strcpy(buf, argv[1]);
+
+	if (target == 0xfacade)
+	{
+		puts("flag{I'm burning inside}");
+	}
+}
