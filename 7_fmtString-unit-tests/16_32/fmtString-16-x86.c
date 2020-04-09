11a623e777e617c68a647e0c94b2e0691ad82cbf
guyinatuxedo <ryancmeinke@gmail.com>
2020-04-09 03:36:00 -0700

    finished basic x86 fmt string attacks

---
+++ 7_fmtString-unit-tests/16_32/fmtString-16-x86.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void notPwned(void)
+{
+	system("/bin/ls");
+}
+
+void main(void)
+{
+	char buf[39];
+	int v0, v1, v2;
+
+        printf("It seems I've been buried alive: %p\n", printf);
+
+	fgets(buf, sizeof(buf), stdin);
+
+	printf(buf);
+
+	exit(0);
+}
